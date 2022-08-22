448d5c360c0cf4b56f3d6a0e9a01038ebfbbb9e6
arif <md.arif.tiens@gmail.com>
2022-08-22 15:40:25 +0600

    added save action for admin and general user for widgets

---
+++ Modules/Backend/Http/Controllers/Widgets/WidgetsSaveActionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Backend\Http\Controllers\Widgets;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class WidgetsSaveActionController extends Controller
+{
+    public function adminSave(Request $request)
+    {
+        $validatedFields = $request->validate([
+            'type'=>'required',
+            'name'=>'required',
+            'fields'=>'array|required',
+        ]);
+
+    }
+
+    public function generalSave(Request $request)
+    {
+        $validatedFields = $request->validate([
+            'type'=>'required',
+            'name'=>'required',
+            'fields'=>'array|required',
+        ]);
+    }
+
+}
