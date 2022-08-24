eab03e0d6cffc4e6f6daf2715dfe4d05ff82631d
arif <md.arif.tiens@gmail.com>
2022-08-24 14:29:22 +0600

    custom widght added

---
+++ Modules/Backend/Resources/views/widgets/admin-view/custom.blade.php
@@ -0,0 +1,22 @@
+
+@if(isset($widgetDetail))
+<div class="mb-3">
+    <label for="bangla_text_or_html" class="form-label">Bangla here (Text/Html)</label>
+    <textarea class="form-control" name="custom[bangla_text_or_html]" id="bangla_text_or_html" required cols="30" rows="10">{{ old('custom')['bangla_text_or_html'] ?? getWidgetCustomFieldValue($widgetDetail->widgetFields,'bangla_text_or_html')  }}</textarea>
+</div>
+<div class="mb-3">
+    <label for="english_text_or_html" class="form-label">English here (Text/Html)</label>
+    <textarea class="form-control" name="custom[english_text_or_html]" id="english_text_or_html" required cols="30" rows="10">{{ old('custom')['english_text_or_html'] ?? getWidgetCustomFieldValue($widgetDetail->widgetFields,'english_text_or_html')  }}</textarea>
+</div>
+@else
+
+    <div class="mb-3">
+        <label for="bangla_text_or_html" class="form-label">Bangla here (Text/Html)</label>
+        <textarea class="form-control" name="custom[bangla_text_or_html]" id="bangla_text_or_html" required cols="30" rows="10">{{ old('custom')['bangla_text_or_html'] ?? ''  }}</textarea>
+    </div>
+    <div class="mb-3">
+        <label for="english_text_or_html" class="form-label">English here (Text/Html)</label>
+        <textarea class="form-control" name="custom[english_text_or_html]" id="english_text_or_html" required cols="30" rows="10">{{ old('custom')['english_text_or_html'] ?? ''  }}</textarea>
+    </div>
+@endif
+
