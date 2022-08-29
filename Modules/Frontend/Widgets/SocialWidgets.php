8a1d1147da6a7dd46e77ddf19f34e1fddf4b63cf
arif <md.arif.tiens@gmail.com>
2022-08-29 16:52:22 +0600

    news , social visitor counter widgets added

---
+++ Modules/Frontend/Widgets/SocialWidgets.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Frontend\Widgets;
+
+use Modules\Frontend\Abstracts\Widget;
+
+class SocialWidgets extends Widget
+{
+
+    function data()
+    {
+
+    }
+}
---
+++ Modules/Frontend/Widgets/VisitorCounterWidgets.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Frontend\Widgets;
+
+use Modules\Frontend\Abstracts\Widget;
+
+class VisitorCounterWidgets extends Widget
+{
+
+    function data()
+    {
+
+    }
+}
