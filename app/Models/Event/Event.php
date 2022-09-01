f2dd1ebcbe86f5e1fd70de222cd3f97789c3ce0f
arif <md.arif.tiens@gmail.com>
2022-09-01 15:53:09 +0600

    update Event model

--- app/Models/Event/Event.php
+++ app/Models/Event/Event.php
@@ -2,13 +2,10 @@
 
 namespace App\Models\Event;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    use HasFactory;
-
     protected $table = 'events';
 
     protected $fillable = [
@@ -23,4 +20,6 @@ class Event extends Model
         'from_datetime',
         'to_datetime'
     ];
+
+
 }
