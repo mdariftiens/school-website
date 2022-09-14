8f16304eb092990cc608342171d56f3ee9ab44ff
mdariftiens <md.arif.tiens@gmail.com>
2022-09-14 16:18:25 +0600

    update helper

--- database/seeders/OptionTableSeeder.php
+++ database/seeders/OptionTableSeeder.php
@@ -18,20 +18,9 @@ class OptionTableSeeder extends Seeder
         Model::unguard();
 
         Option::create([
-            'name' => 'site_name',
-            'value' => 'School website',
-        ]);
-
-        Option::create([
-            'name' => 'site_tagline',
-            'value' => 'Best School website',
-        ]);
-
-        Option::create([
             'name' => 'current_theme_id',
             'value' => 'primary-theme',
         ]);
-
 
     }
 }
