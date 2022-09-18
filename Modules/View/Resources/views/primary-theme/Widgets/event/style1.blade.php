80b3a5c99a1689db4092081d7cfd0e81cce8d442
mdariftiens <md.arif.tiens@gmail.com>
2022-09-18 14:13:34 +0600

    adding Template in Frontend View.

---
+++ Modules/View/Resources/views/primary-theme/Widgets/event/style1.blade.php
@@ -0,0 +1,41 @@
+<div class="event_list_area">
+    <div
+        class="title text-titleColor flex justify-between shadow-[0px_2px_2px_2px_rgba(0,0,0,0.1)] items-center pl-2 pr-2 pt-2 pb-2 mb-4">
+        <h3 class="text-[25px] font-semibold">Events</h3>
+    </div>
+    <div class="event_list_content border border-[#ddd] pt-3 pb-3 font-lato h-80 overflow-auto">
+        <div class="grid grid-cols-1 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
+            <div>
+                <a href="#">
+                    <p class="text-[17px] bg-[#FF7F50] text-[#fff] px-3 py-2 mb-2 inline-block font-semibold shadow-[0px_2px_2px_2px_rgba(0,0,0,0.1)] rounded-md">
+                        <strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022, 5:35 AM</p>
+                    <p class="text-[17px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
+                    <p class="text-lg font-medium">It is a long established fact that a reader will be
+                        distracted by the readable content of a page when looking at its layout...</p>
+                </a>
+            </div>
+        </div><!--news list item-->
+        <div class="grid grid-cols-1 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
+            <div>
+                <a href="#">
+                    <p class="text-[17px] bg-[#FF7F50] text-[#fff] px-3 py-2 mb-2 inline-block font-semibold shadow-[0px_2px_2px_2px_rgba(0,0,0,0.1)] rounded-md">
+                        <strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022, 5:35 AM</p>
+                    <p class="text-[17px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
+                    <p class="text-lg font-medium">It is a long established fact that a reader will be
+                        distracted by the readable content of a page when looking at its layout...</p>
+                </a>
+            </div>
+        </div><!--news list item-->
+        <div class="grid grid-cols-1 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
+            <div>
+                <a href="#">
+                    <p class="text-[17px] bg-[#FF7F50] text-[#fff] px-3 py-2 mb-2 inline-block font-semibold shadow-[0px_2px_2px_2px_rgba(0,0,0,0.1)] rounded-md">
+                        <strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022, 5:35 AM</p>
+                    <p class="text-[17px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
+                    <p class="text-lg font-medium">It is a long established fact that a reader will be
+                        distracted by the readable content of a page when looking at its layout...</p>
+                </a>
+            </div>
+        </div><!--news list item-->
+    </div>
+</div>
--- Modules/View/Resources/views/primary-theme/Widgets/news/default.blade.php
+++ Modules/View/Resources/views/primary-theme/Widgets/news/default.blade.php
@@ -1,9 +1,9 @@
-<div class="event_list_area">
+<div class="news_list_area">
     <div
         class="title text-titleColor flex justify-between shadow-[0px_2px_2px_2px_rgba(0,0,0,0.1)] items-center pl-2 pr-2 pt-2 pb-2 mb-4">
-        <h3 class="text-[25px] font-semibold">Events</h3>
+        <h3 class="text-[25px] font-semibold">News</h3>
     </div>
-    <div class="event_list_content border border-[#ddd] pt-3 pb-3 font-lato h-80 overflow-auto">
+    <div class="news_list_content border border-[#ddd] pt-3 pb-3 font-lato h-80 overflow-auto">
         <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
             <div class="col-span-2 mr-3">
                 <a href="#">
@@ -13,10 +13,8 @@
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-semibold">Shaheed Bir Uttam Lt Anwar Girls’ College</p>
-                    <p class="text-[16px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
-                    <p class="text-[15px]"><strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022,
-                        5:35 AM</p>
+                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
+                        of</p>
                 </a>
             </div>
         </div><!--news list item-->
@@ -29,10 +27,8 @@
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-semibold">Shaheed Bir Uttam Lt Anwar Girls’ College</p>
-                    <p class="text-[16px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
-                    <p class="text-[15px]"><strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022,
-                        5:35 AM</p>
+                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
+                        of</p>
                 </a>
             </div>
         </div><!--news list item-->
@@ -45,10 +41,8 @@
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-semibold">Shaheed Bir Uttam Lt Anwar Girls’ College</p>
-                    <p class="text-[16px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
-                    <p class="text-[15px]"><strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022,
-                        5:35 AM</p>
+                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
+                        of</p>
                 </a>
             </div>
         </div><!--news list item-->
@@ -61,10 +55,8 @@
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-semibold">Shaheed Bir Uttam Lt Anwar Girls’ College</p>
-                    <p class="text-[16px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
-                    <p class="text-[15px]"><strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022,
-                        5:35 AM</p>
+                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
+                        of</p>
                 </a>
             </div>
         </div><!--news list item-->
@@ -77,10 +69,8 @@
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-semibold">Shaheed Bir Uttam Lt Anwar Girls’ College</p>
-                    <p class="text-[16px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
-                    <p class="text-[15px]"><strong>Date :</strong> Aug 15, 2022, 9:35 AM - Aug 15, 2022,
-                        5:35 AM</p>
+                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
+                        of</p>
                 </a>
             </div>
         </div><!--news list item-->
@@ -93,10 +83,8 @@
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-semibold">Shaheed Bir Uttam Lt Anwar Girls’ College</p>
-                    <p class="text-[16px]"><strong>Address :</strong> Bonani, Dhaka-1205</p>
-                    <p class="text-[15px]"><strong>Date :</strong> Aug 15, 2022, 9:35:00 AM - Aug 15,
-                        2022, 5:35:00 AM</p>
+                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
+                        of</p>
                 </a>
             </div>
         </div><!--news list item-->
