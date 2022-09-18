d89aaa28120f062eb7b58d2b9db61d335c3b4a82
mdariftiens <md.arif.tiens@gmail.com>
2022-09-18 16:34:54 +0600

    update code   Left and Right sidebar for Homepage

---
+++ Modules/View/Resources/views/primary-theme/template/sidebar/left/default.blade.php
@@ -0,0 +1,11 @@
+<div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
+    <!--Sidebar area start-->
+    <div class="sidebar_area">
+        @if( isHomepageLeftSidebarVisible() )
+            <!-- left sidebar container -->
+        {!! getSidebarWithWidgets('home-left-sidebar') !!}
+        @endif
+    </div>
+    <!--Sidebar area end-->
+
+</div><!--Sidebar content-->
