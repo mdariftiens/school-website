0e5001dc00b48ec30fb0650a8932a15d3f0e48f1
mdariftiens <md.arif.tiens@gmail.com>
2022-09-19 10:48:01 +0600

    update sidebar code

--- Modules/View/Resources/views/primary-theme/template/sidebar/left/default.blade.php
+++ Modules/View/Resources/views/primary-theme/template/sidebar/left/default.blade.php
@@ -1,11 +1,12 @@
-<div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
+@if( isHomepageLeftSidebarVisible() )
+
+    <div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
     <!--Sidebar area start-->
     <div class="sidebar_area">
-        @if( isHomepageLeftSidebarVisible() )
-            <!-- left sidebar container -->
+        <!-- left sidebar container -->
         {!! getSidebarWithWidgets('home-left-sidebar') !!}
-        @endif
     </div>
     <!--Sidebar area end-->
 
 </div><!--Sidebar content-->
+@endif
--- Modules/View/Resources/views/primary-theme/template/sidebar/right/default.blade.php
+++ Modules/View/Resources/views/primary-theme/template/sidebar/right/default.blade.php
@@ -1,12 +1,12 @@
-<div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
+@if( isHomepageRightSidebarVisible() )
+
+    <div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
     <!--Sidebar area start-->
     <div class="sidebar_area">
-
-        @if( isHomepageRightSidebarVisible() )
-            <!-- Right sidebar container -->
+        <!-- Right sidebar container -->
         {!! getSidebarWithWidgets('home-right-sidebar') !!}
-        @endif
     </div>
     <!--Sidebar area end-->
 
 </div><!--Sidebar content-->
+@endif
