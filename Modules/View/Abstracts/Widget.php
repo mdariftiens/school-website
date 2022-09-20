9baee5f5d2c21a283131874a59a9bcb1cdccd332
mdariftiens <md.arif.tiens@gmail.com>
2022-09-20 17:10:42 +0600

    update widget abstract class

--- Modules/View/Abstracts/Widget.php
+++ Modules/View/Abstracts/Widget.php
@@ -4,16 +4,18 @@ namespace Modules\View\Abstracts;
 
 abstract class Widget
 {
+    public $widgetWithWidgetDetail;
 
     abstract  function data();
 
     public  function show($widgetWithWidgetDetail)
     {
+        $this->widgetWithWidgetDetail = $widgetWithWidgetDetail;
+
         $data = [
             'widgetWithWidgetDetail' => $widgetWithWidgetDetail,
             'data' => $this->data()
         ];
-
         $view = "view::".getCurrentThemeId().'/Widgets/'  . $widgetWithWidgetDetail->type . '/' . $widgetWithWidgetDetail->header_template;
 
         return view($view, $data);
