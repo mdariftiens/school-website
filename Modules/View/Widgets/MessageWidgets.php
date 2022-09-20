8e9b28b5beb1bf4797f95148f2b32398c00c7e57
mdariftiens <md.arif.tiens@gmail.com>
2022-09-20 17:11:12 +0600

    update Message Widget data return

--- Modules/View/Widgets/MessageWidgets.php
+++ Modules/View/Widgets/MessageWidgets.php
@@ -2,6 +2,7 @@
 
 namespace Modules\View\Widgets;
 
+use App\Models\Message\Message;
 use Modules\View\Abstracts\Widget;
 
 class MessageWidgets extends Widget
@@ -9,6 +10,7 @@ class MessageWidgets extends Widget
 
     function data()
     {
-
+        $numberOfMessageShow = getWidgetCustomFieldValue($this->widgetWithWidgetDetail->widgetFields,'number_of_notice_show');
+        return Message::take($numberOfMessageShow)->get();
     }
 }
