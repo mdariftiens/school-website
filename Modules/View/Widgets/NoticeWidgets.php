906684d39ecba4f4d89be75645279c590a8ba07a
mdariftiens <md.arif.tiens@gmail.com>
2022-09-20 18:30:28 +0600

    notice widget data

--- Modules/View/Widgets/NoticeWidgets.php
+++ Modules/View/Widgets/NoticeWidgets.php
@@ -2,6 +2,7 @@
 
 namespace Modules\View\Widgets;
 
+use App\Models\Notice\Notice;
 use Modules\View\Abstracts\Widget;
 
 class NoticeWidgets extends Widget
@@ -9,6 +10,8 @@ class NoticeWidgets extends Widget
 
     function data()
     {
-
+        $limitNoticeNumber = getWidgetCustomFieldValue($this->widgetWithWidgetDetail->widgetFields, 'limit_notice_number');
+        $noticeHeight = getWidgetCustomFieldValue($this->widgetWithWidgetDetail->widgetFields, 'notice_height');
+        return Notice::take($limitNoticeNumber)->get();
     }
 }
