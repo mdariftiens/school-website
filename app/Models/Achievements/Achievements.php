958db379373a15361abe2b3dc43dc7aaa2ef904a
mdariftiens <md.arif.tiens@gmail.com>
2022-09-20 18:37:52 +0600

    news and achievement model added

---
+++ app/Models/Achievements/Achievements.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Achievements;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Achievements extends Model
+{
+    use HasFactory;
+
+    protected $table = 'achievements';
+
+    protected $fillable = [
+        'bangla_title',
+        'english_title',
+        'featured_image_link',
+        'bangla_detail',
+        'english_detail',
+    ];
+}
---
+++ app/Models/News/News.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\News;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory;
+
+    protected $table = 'news';
+
+    protected $fillable = [
+        'bangla_title',
+        'english_title',
+        'featured_image_link',
+        'bangla_detail',
+        'english_detail',
+    ];
+}
