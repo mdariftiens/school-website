adf3f7b435992189bfdf30f7df579488e648307f
mdariftiens <md.arif.tiens@gmail.com>
2022-09-20 18:35:29 +0600

    news and achievement migration added

---
+++ database/migrations/2022_09_20_123249_create_achievements_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAchievementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('bangla_title');
+            $table->string('english_title');
+            $table->string('featured_image_link');
+            $table->string('bangla_detail');
+            $table->string('english_detail');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('achievements');
+    }
+}
