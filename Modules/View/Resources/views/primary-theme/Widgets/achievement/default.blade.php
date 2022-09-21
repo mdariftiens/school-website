d5ba51ee49ed92f47d0f3fff6197fbed4eefd0e5
mdariftiens <md.arif.tiens@gmail.com>
2022-09-21 16:54:30 +0600

    achievement widget dynamic

--- Modules/View/Resources/views/primary-theme/Widgets/achievement/default.blade.php
+++ Modules/View/Resources/views/primary-theme/Widgets/achievement/default.blade.php
@@ -1,92 +1,27 @@
 <div class="achievement_list_area">
     <div
         class="title text-titleColor flex justify-between shadow-[0px_2px_2px_2px_rgba(0,0,0,0.1)] items-center pl-2 pr-2 pt-2 pb-2 mb-4">
-        <h3 class="text-[25px] font-semibold">Our Achievements</h3>
+        <h3 class="text-[25px] font-semibold">{{ $widgetWithWidgetDetail->english_title }}</h3>
     </div>
     <div class="_list_content border border-[#ddd] pt-3 pb-3 font-lato h-80 overflow-auto">
+        @foreach($data as $row)
         <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
             <div class="col-span-2 mr-3">
                 <a href="#">
                     <img class="w-full mx-auto border border-[#ddd] p-1"
-                         src="https://sagc.edu.bd/wp-content/uploads/2022/03/Ch_Sir.jpg" alt="image">
+                         src="{{ $row->featured_image_link }}"
+                         alt="{{ $row->english_title }}"
+                         title="{{ $row->english_title }}"
+                    >
+                    {{ $row->english_title }}
                 </a>
             </div>
             <div class="col-span-9">
                 <a href="#">
-                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
-                        of</p>
+                    <p class="text-lg font-medium">{{ $row->english_title }}</p>
                 </a>
             </div>
         </div><!--news list item-->
-        <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
-            <div class="col-span-2 mr-3">
-                <a href="#">
-                    <img class="w-full mx-auto border border-[#ddd] p-1"
-                         src="https://sagc.edu.bd/wp-content/uploads/2022/03/Ch_Sir.jpg" alt="image">
-                </a>
-            </div>
-            <div class="col-span-9">
-                <a href="#">
-                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
-                        of</p>
-                </a>
-            </div>
-        </div><!--news list item-->
-        <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
-            <div class="col-span-2 mr-3">
-                <a href="#">
-                    <img class="w-full mx-auto border border-[#ddd] p-1"
-                         src="https://sagc.edu.bd/wp-content/uploads/2022/03/Ch_Sir.jpg" alt="image">
-                </a>
-            </div>
-            <div class="col-span-9">
-                <a href="#">
-                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
-                        of</p>
-                </a>
-            </div>
-        </div><!--news list item-->
-        <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
-            <div class="col-span-2 mr-3">
-                <a href="#">
-                    <img class="w-full mx-auto border border-[#ddd] p-1"
-                         src="https://sagc.edu.bd/wp-content/uploads/2022/03/Ch_Sir.jpg" alt="image">
-                </a>
-            </div>
-            <div class="col-span-9">
-                <a href="#">
-                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
-                        of</p>
-                </a>
-            </div>
-        </div><!--news list item-->
-        <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
-            <div class="col-span-2 mr-3">
-                <a href="#">
-                    <img class="w-full mx-auto border border-[#ddd] p-1"
-                         src="https://sagc.edu.bd/wp-content/uploads/2022/03/Ch_Sir.jpg" alt="image">
-                </a>
-            </div>
-            <div class="col-span-9">
-                <a href="#">
-                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
-                        of</p>
-                </a>
-            </div>
-        </div><!--news list item-->
-        <div class="grid grid-cols-11 items-center ml-2 mr-2 mb-3 border-b border-[#ddd] pb-3">
-            <div class="col-span-2 mr-3">
-                <a href="#">
-                    <img class="w-full mx-auto border border-[#ddd] p-1"
-                         src="https://sagc.edu.bd/wp-content/uploads/2022/03/Ch_Sir.jpg" alt="image">
-                </a>
-            </div>
-            <div class="col-span-9">
-                <a href="#">
-                    <p class="text-lg font-medium">Shaheed Bir Uttam Lt Anwar Girls’ College is one
-                        of</p>
-                </a>
-            </div>
-        </div><!--news list item-->
+        @endforeach
     </div>
 </div>
