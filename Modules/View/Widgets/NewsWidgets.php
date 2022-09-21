08ab7f1f1df5515839bf02f8cfffa656612e4b5e
mdariftiens <md.arif.tiens@gmail.com>
2022-09-21 16:37:03 +0600

    all news widget dynamic

--- Modules/View/Widgets/NewsWidgets.php
+++ Modules/View/Widgets/NewsWidgets.php
@@ -2,6 +2,7 @@
 
 namespace Modules\View\Widgets;
 
+use App\Models\News\News;
 use Modules\View\Abstracts\Widget;
 
 class NewsWidgets extends Widget
@@ -9,6 +10,7 @@ class NewsWidgets extends Widget
 
     function data()
     {
-
+        $limitNewsNumber = (int) getWidgetCustomFieldValue($this->widgetWithWidgetDetail->widgetFields,'limit_news_number');
+        return News::latest()->take($limitNewsNumber)->get();
     }
 }
