a84b4074b002c6a67d23d3ee48a9801610c0d55a
mdariftiens <md.arif.tiens@gmail.com>
2022-09-21 16:48:28 +0600

    achievement.blade.php add new filed for limit

--- Modules/Admin/Resources/views/widgets/admin-view/achievement.blade.php
+++ Modules/Admin/Resources/views/widgets/admin-view/achievement.blade.php
@@ -1,5 +1,18 @@
 
 @if(isset($widgetDetail))
+    <div class="mb-3">
+        <label for="limit_notice_number" class="form-label">Limit Achievement Number</label>
+        <input type="number" class="form-control" name="achievement[limit_achievement_number]" id="limit_achievement_number"
+               value="{{ old('notice')['limit_achievement_number'] ?? getWidgetCustomFieldValue($widgetDetail->widgetFields,'limit_achievement_number')  }}"
+        >
+    </div>
 @else
+    <div class="mb-3">
+        <label for="limit_notice_number" class="form-label">Limit notice number</label>
+        <input type="number" class="form-control" name="achievement[limit_achievement_number]" id="limit_achievement_number"
+               required
+               value="{{ old('notice')['limit_achievement_number'] ?? 5  }}"
+        >
+    </div>
 @endif
 
