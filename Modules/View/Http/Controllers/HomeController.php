187f4fc91ae828368c955a5795c4e580ebd39898
mdariftiens <md.arif.tiens@gmail.com>
2022-09-25 13:46:21 +0600

    update home page view.

--- Modules/View/Http/Controllers/HomeController.php
+++ Modules/View/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('view::'.getCurrentThemeId().'.index');
+        return view('view::'.getCurrentThemeId().'.home');
     }
 
     /**
