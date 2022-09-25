23cf26837cc333d7a038aba96868ff59117b7b6f
mdariftiens <md.arif.tiens@gmail.com>
2022-09-25 17:53:03 +0600

    left and right sidebar update for Homepage and General page. :)

--- Modules/View/Resources/views/primary-theme/template/sidebar/left/default.blade.php
+++ Modules/View/Resources/views/primary-theme/template/sidebar/left/default.blade.php
@@ -4,11 +4,20 @@
     <!--Sidebar area start-->
     <div class="sidebar_area">
         <!-- left sidebar container -->
-        @if(getCurrentRouteName()=='home')
-            {!! getSidebarWithWidgets('home-left-sidebar') !!}
-        @else
-            {!! getSidebarWithWidgets('general-left-sidebar') !!}
-        @endif
+        {!! getSidebarWithWidgets('home-left-sidebar') !!}
+    </div>
+    <!--Sidebar area end-->
+
+</div><!--Sidebar content-->
+@endif
+
+@if( isGeneralPageLeftSidebarVisible() )
+
+    <div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
+    <!--Sidebar area start-->
+    <div class="sidebar_area">
+        <!-- left sidebar container -->
+        {!! getSidebarWithWidgets('general-left-sidebar') !!}
     </div>
     <!--Sidebar area end-->
 
--- Modules/View/Resources/views/primary-theme/template/sidebar/right/default.blade.php
+++ Modules/View/Resources/views/primary-theme/template/sidebar/right/default.blade.php
@@ -4,13 +4,23 @@
     <!--Sidebar area start-->
     <div class="sidebar_area">
         <!-- Right sidebar container -->
-        @if(getCurrentRouteName()=='home')
-            {!! getSidebarWithWidgets('home-right-sidebar') !!}
-        @else
-            {!! getSidebarWithWidgets('general-right-sidebar') !!}
-        @endif
+        {!! getSidebarWithWidgets('home-right-sidebar') !!}
+
     </div>
     <!--Sidebar area end-->
 
 </div><!--Sidebar content-->
+@endif
+
+@if( isGeneralPageRightSidebarVisible() )
+
+    <div class="sm:w-full  md:w-1/4 lg:w-1/3 xl:w-1/3 2xl:w-1/3">
+        <!--Sidebar area start-->
+        <div class="sidebar_area">
+            <!-- Right sidebar container -->
+            {!! getSidebarWithWidgets('general-right-sidebar') !!}
+        </div>
+        <!--Sidebar area end-->
+
+    </div><!--Sidebar content-->
 @endif
