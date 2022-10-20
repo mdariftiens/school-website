4e2c40d4ff85f604a30c7b5d7deb756bf3fd8824
MD. Jahangir Alom Pial <cseselimreza@gmail.com>
2022-10-20 13:47:16 +0600

    contact page redesing and translatable issue id #19

--- Modules/View/Resources/views/primary-theme/contactus.blade.php
+++ Modules/View/Resources/views/primary-theme/contactus.blade.php
@@ -13,7 +13,7 @@
                     <div class="contactus_content flex border border-[#ddd] pt-2 pb-2">
                         <div class="w-full ml-2 mr-2">
                             <h2 class="bg-bgColor text-center text-lg font-semibold text-white pt-1 pb-1 mb-5">
-                                Contact Us
+                                {{ __("Contact Us") }}
                             </h2>
                             <div class="grid grid-cols-2 gap-5 sm:grid-cols-1 md:grid-cols-2">
                                 {!! getThemeSettingValue('_theme_setting_google_map') !!}
@@ -35,30 +35,30 @@
                                             @csrf
 
                                             <div class="mb-6">
-                                                <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Name *</label>
-                                                <input type="text" name="name" id="name" placeholder="Name" value="{{ old('name') }}" required="" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
+                                                <label for="name" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('name') }}: *</label>
+                                                <input type="text" name="name" id="name" placeholder="{{ __("your name") }}" value="{{ old('name') }}" required="" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
                                             </div>
 
                                             <div class="mb-3">
-                                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">Email</label>
-                                                <input type="email" id="email" name="email" placeholder="Email" value="{{ old('email') }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
+                                                <label for="email" class="block mb-2 text-sm text-gray-600 dark:text-gray-400">{{ __('email') }}:</label>
+                                                <input type="email" id="email" name="email" placeholder="{{ __("enter email") }}" value="{{ old('email') }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
                                             </div>
 
                                             <div class="mb-3">
-                                                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400" for="phone">phone</label>
-                                                <input type="text" id="phone" name="phone" placeholder="Phone" value="{{ old('phone') }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
+                                                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400" for="phone">{{ __('phone') }}:</label>
+                                                <input type="text" id="phone" name="phone" placeholder="{{ __("enter phone") }}" value="{{ old('phone') }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
                                             </div>
 
                                             <div class="mb-3">
-                                                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400" for="subject">Subject *</label>
-                                                <input type="text" id="subject" name="subject" placeholder="Subject" required value="{{ old('subject') }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
+                                                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400" for="subject">{{ __('subject') }}: *</label>
+                                                <input type="text" id="subject" name="subject" placeholder="{{ __("enter subject") }}" required value="{{ old('subject') }}" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">
                                             </div>
 
                                             <div class="mb-3">
-                                                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400" for="message">Message</label> <br>
+                                                <label class="block mb-2 text-sm text-gray-600 dark:text-gray-400" for="message">{{ __('message') }}:</label>
                                                 <textarea name="message" id="message" cols="30" rows="10" required class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-300 dark:text-white dark:border-gray-600">{{ old('message') }}</textarea>
                                             </div>
-                                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Submit</button>
+                                            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">{{ __('submit') }}</button>
                                         </form>
                                 </div>
                             </div>
